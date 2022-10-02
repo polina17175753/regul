95df7ba290f3da25594f270e8905e1899790e425
polina17175753 <112984326+polina17175753@users.noreply.github.com>
2022-10-02 21:42:32 +0300

    Add files via upload

---
+++ src/Zavtrak.java
@@ -0,0 +1,12 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Zavtrak {
+    public static void Zavtrak(String input) {
+        input = "Завтрак в 09:00 ";
+        Pattern pattern = Pattern.compile("(2[0-3]|[0-1]\\d):[0-5]\\d");
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
